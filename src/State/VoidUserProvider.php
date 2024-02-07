31a154ce8367bc32587ee5a0734979105026a43b
roadiz-ci <ambroise+ci@roadiz.io>
2024-02-07 11:33:14 +0000

    Merge branch hotfix/v2.2.2

---
+++ src/State/VoidUserProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\UserBundle\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use RZ\Roadiz\UserBundle\Api\Dto\VoidOutput;
+
+final class VoidUserProvider implements ProviderInterface
+{
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): VoidOutput
+    {
+        return new VoidOutput();
+    }
+}
