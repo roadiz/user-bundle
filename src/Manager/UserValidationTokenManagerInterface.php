49283b9ad882c4a952874190ca8031de46300dfa
roadiz-ci <ambroise+ci@roadiz.io>
2024-09-16 13:42:54 +0000

    chore: bumped

--- src/Manager/UserValidationTokenManagerInterface.php
+++ src/Manager/UserValidationTokenManagerInterface.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\UserBundle\Manager;
 
+use RZ\Roadiz\CoreBundle\Entity\User;
 use RZ\Roadiz\UserBundle\Entity\UserValidationToken;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 interface UserValidationTokenManagerInterface
 {
-    public function createForUser(UserInterface $user): UserValidationToken;
+    public function createForUser(User $user): UserValidationToken;
     public function isUserEmailValidated(UserInterface $user): bool;
 }
