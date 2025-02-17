d0626c358842a200d9aae5f95e5a4d742b6750e3
roadiz-ci <ambroise+ci@roadiz.io>
2025-02-17 20:53:23 +0000

    Merge branch hotfix/v2.4.16

--- src/State/PasswordlessUserSignupProcessor.php
+++ src/State/PasswordlessUserSignupProcessor.php
@@ -9,22 +9,25 @@ use ApiPlatform\State\ProcessorInterface;
 use ApiPlatform\Validator\ValidatorInterface;
 use RZ\Roadiz\CoreBundle\Bag\Roles;
 use RZ\Roadiz\CoreBundle\Form\Constraint\RecaptchaServiceInterface;
-use RZ\Roadiz\UserBundle\Api\Dto\UserInput;
+use RZ\Roadiz\CoreBundle\Security\LoginLink\LoginLinkSenderInterface;
+use RZ\Roadiz\UserBundle\Api\Dto\PasswordlessUserInput;
 use RZ\Roadiz\UserBundle\Api\Dto\VoidOutput;
-use RZ\Roadiz\UserBundle\Event\UserSignedUp;
+use RZ\Roadiz\UserBundle\Event\PasswordlessUserSignedUp;
 use RZ\Roadiz\UserBundle\Manager\UserMetadataManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
-final readonly class UserSignupProcessor implements ProcessorInterface
+final readonly class PasswordlessUserSignupProcessor implements ProcessorInterface
 {
     use RecaptchaProtectedTrait;
     use SignupProcessorTrait;
 
     public function __construct(
+        private LoginLinkHandlerInterface $loginLinkHandler,
         private ValidatorInterface $validator,
         private Security $security,
         private RequestStack $requestStack,
@@ -34,7 +37,9 @@ final readonly class UserSignupProcessor implements ProcessorInterface
         private ProcessorInterface $persistProcessor,
         private UserMetadataManagerInterface $userMetadataManager,
         private Roles $rolesBag,
+        private LoginLinkSenderInterface $loginLinkSender,
         private string $publicUserRoleName,
+        private string $passwordlessUserRoleName,
         private string $recaptchaHeaderName = 'x-g-recaptcha-response',
     ) {
     }
@@ -61,23 +66,25 @@ final readonly class UserSignupProcessor implements ProcessorInterface
 
     public function process($data, Operation $operation, array $uriVariables = [], array $context = []): VoidOutput
     {
-        if (!$data instanceof UserInput) {
+        if (!$data instanceof PasswordlessUserInput) {
             throw new BadRequestHttpException(sprintf('Cannot process %s', get_class($data)));
         }
-
         $request = $this->requestStack->getCurrentRequest();
         $this->ValidateRequest($request);
         $this->validateRecaptchaHeader($request);
 
         $user = $this->createUser($data);
-        $user->setPlainPassword($data->plainPassword);
         $user->addRoleEntity($this->rolesBag->get($this->publicUserRoleName));
-        $user->sendCreationConfirmationEmail(true);
+        $user->addRoleEntity($this->rolesBag->get($this->passwordlessUserRoleName));
+        /*
+         * We don't want to send an email right now, we will send a login link instead.
+         */
+        $user->sendCreationConfirmationEmail(false);
         $user->setLocale($request->getLocale());
 
         $this->validator->validate($user);
 
-        $this->eventDispatcher->dispatch(new UserSignedUp($user));
+        $this->eventDispatcher->dispatch(new PasswordlessUserSignedUp($user));
         // Process and persist user to database before returning a VoidOutput
         $user = $this->persistProcessor->process($user, $operation, $uriVariables, $context);
 
@@ -86,6 +93,12 @@ final readonly class UserSignupProcessor implements ProcessorInterface
             $userMetadata->setMetadata($data->metadata);
             $this->persistProcessor->process($userMetadata, $operation, $uriVariables, $context);
         }
+
+        /*
+         * Send user first login link, this will also set user as EMAIL_VALIDATED
+         */
+        $loginLinkDetails = $this->loginLinkHandler->createLoginLink($user, $request);
+        $this->loginLinkSender->sendLoginLink($user, $loginLinkDetails);
 
         return new VoidOutput();
     }
