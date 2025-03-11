603999082344d8284f5263d9cc195ae519616945
roadiz-ci <ambroise+ci@roadiz.io>
2025-03-11 17:23:28 +0000

    Merge branch release/v2.5.0

--- src/Api/Dto/UserInput.php
+++ src/Api/Dto/UserInput.php
@@ -6,22 +6,10 @@ namespace RZ\Roadiz\UserBundle\Api\Dto;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
-final class UserInput
+final class UserInput extends AbstractUserInput
 {
-    #[Assert\Email]
-    #[Assert\NotNull]
-    public string $email = '';
-
     #[Assert\NotNull]
     #[Assert\NotBlank]
     #[Assert\NotCompromisedPassword()]
     public string $plainPassword = '';
-    public ?string $publicName = null;
-    public ?string $firstName = null;
-    public ?string $lastName = null;
-    public ?string $phone = null;
-    public ?string $company = null;
-    public ?string $job = null;
-    public ?\DateTime $birthday = null;
-    public ?array $metadata = null;
 }
