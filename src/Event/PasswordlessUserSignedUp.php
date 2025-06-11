f56a9190e6884df415f626b642549fcfb9320d15
roadiz-ci <ambroise+ci@roadiz.io>
2025-06-11 17:46:03 +0000

    Merge branch hotfix/v2.5.21

---
+++ src/Event/PasswordlessUserSignedUp.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\UserBundle\Event;
+
+use RZ\Roadiz\CoreBundle\Event\FilterUserEvent;
+
+class PasswordlessUserSignedUp extends FilterUserEvent
+{
+}
