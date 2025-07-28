b7cd73356d08c22cc889633209d583f7a06d4db3
roadiz-ci <ambroise+ci@roadiz.io>
2025-07-28 13:52:45 +0000

    feat!: Upgrade Symfony to version 7.3 (#154)

--- src/Api/Dto/UserInput.php
+++ src/Api/Dto/UserInput.php
@@ -10,7 +10,8 @@ final class UserInput extends AbstractUserInput
 {
     #[Assert\NotNull]
     #[Assert\NotBlank]
-    #[Assert\Length(min: 8, max: 120)]
+    #[Assert\Length(min: 12, max: 120)]
     #[Assert\NotCompromisedPassword()]
+    #[Assert\PasswordStrength(minScore: Assert\PasswordStrength::STRENGTH_MEDIUM)]
     public string $plainPassword = '';
 }
