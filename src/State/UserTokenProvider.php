bcc531913710f230e261632401919c46c6592b96
roadiz-ci <ambroise+ci@roadiz.io>
2025-10-28 09:05:23 +0000

    chore: Bumped

--- src/State/UserTokenProvider.php
+++ src/State/UserTokenProvider.php
@@ -24,7 +24,6 @@ final readonly class UserTokenProvider implements ProviderInterface
     ) {
     }
 
-    #[\Override]
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): UserOutput
     {
         $user = $this->security->getUser();
