591b5e29d4db86cf78f6a9329cee3ab3ecf99bdd
roadiz-ci <ambroise+ci@roadiz.io>
2026-05-26 15:36:58 +0000

    chore: bumped

--- src/Manager/UserValidationTokenManager.php
+++ src/Manager/UserValidationTokenManager.php
@@ -105,6 +105,6 @@ final readonly class UserValidationTokenManager implements UserValidationTokenMa
             )
         );
 
-        $this->notifier->send($notification, new Recipient($user->getEmail() ?? throw new \RuntimeException('User email is null.')));
+        $this->notifier->send($notification, new Recipient($user->getEmail()));
     }
 }
